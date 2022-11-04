e96e62cf9c51cccf8aa5276b936e5939b0754d9b
T-Punti <72748202+T-Punti@users.noreply.github.com>
2022-11-04 10:30:19 +0100

    stand 4.11.2022

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UserList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Userlist from './components/UserList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    UserList,
+  },
+};
 </script>
 
 <style>
@@ -21,7 +20,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/UserList.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Users</h1>
+
+</template>
+
+<script>
+import { UseFetch } from '../composable/UseFetch.js';
+
+export default {
+  setup(){
+    const {data,error,loading} = UseFetch('https://jsonplaceholder.typicode.com/users',{});
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
